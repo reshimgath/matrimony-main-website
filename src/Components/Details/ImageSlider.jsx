43694532edf7d7dabdd8b8449ed68aad75262358
Aakash Jadhav <aakash2jadhav@gmail.com>
2023-07-24 20:35:24 +0530

    Add singlePageProgileUpdate Component

--- src/Components/Details/ImageSlider.jsx
+++ src/Components/Details/ImageSlider.jsx
@@ -21,11 +21,6 @@ const ImageSlider = ({ img1, img2, img3 }) => {
 
   return (
     <>
-      {images.length > 0 && (
-        <div className="text-center">
-          <h1>Images </h1>
-        </div>
-      )}
       <div className="row justify-content-center">
         <div className="col-md-6 col-lg-5">
           <div className="imgslider mx-5 mt-4  ">
