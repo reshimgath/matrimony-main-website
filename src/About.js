534d0e63227700e09c3720653ca145c75a6e20b0
reshimgath <121487990+reshimgath@users.noreply.github.com>
2023-10-06 18:45:39 +0530

    Update About.js

--- src/About.js
+++ src/About.js
@@ -23,26 +23,10 @@ const About = () => {
 
                     <div className="col-lg-6">
                         <div className='about_description container mb-5'>
-                            <p style={{ textAlign: 'justify' }}>
-                                <em>
-                                    <b>Reshimgath Matrimony</b>, the leading matrimonial website that assists individuals in finding their right life partners, began its journey in <b>2018</b> by <b>Mr. Jishan Shaikh</b>, with the goal of creating a platform that provides a secure and trustworthy space for people to meet their soulmates.
-                                </em>
+                            <p style={{ textAlign: 'center' }}>
+Abouts us goes here....
                             </p>
-                            <p style={{ textAlign: 'justify' }}>
-                                <em>
-                                    <b>Reshimgath Matrimony</b> provides a range of features that allow our users to filter and search for potential partners based on their preferences, such as age, religion, location, and more. We also offer personalized matchmaking services for those who require additional assistance in finding their perfect match.
-                                </em>
-                            </p>
-                            <p style={{ textAlign: 'justify' }}>
-                                <em>
-                                    At <b>Reshimgath Matrimony</b>, we are committed to providing exceptional customer service to our users. Our dedicated team is available 24/7 to assist users with any queries or concerns they may have.
-                                </em>
-                            </p>
-                            <p style={{ textAlign: 'justify' }}>
-                                <em>
-                                    We believe that finding a life partner is one of the most significant decisions a person can make, and we are honored to be a part of this journey. Join us today and find the love of your life on Reshimgath.
-                                </em>
-                            </p>
+                            
                         </div>
                     </div>
                 </div>
